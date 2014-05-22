626f070feee6a7dbd9bac27050c14ee052832816
Dustin Moorman <dustin.moorman@gmail.com>
2014-05-21 22:27:01 -0500

    menu building

--- app/lib/flashcards.rb
+++ app/lib/flashcards.rb
@@ -7,6 +7,19 @@ require_relative 'clui'
 
 clui = Clui.new
 
+loop do
 clui.getMenu
-
 uin = gets.chomp
+  case uin
+    when '1'
+      clui.messenger.push('user chose 1')
+    when '2'
+      clui.messenger.push('user chose 2')
+    when '3'
+      clui.messenger.push('user chose 3')
+    when '4'
+      break
+    else
+      clui.messenger.push('try again')
+  end
+end
