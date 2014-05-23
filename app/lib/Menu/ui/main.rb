4c73f0344cc8be84e7f763e4b68d4346aefdb6bf
Dustin Moorman <dustin.moorman@gmail.com>
2014-05-22 21:41:46 -0500

    a

--- app/lib/Menu/ui/main.rb
+++ app/lib/Menu/ui/main.rb
@@ -1,2 +1,36 @@
+autoload :Menu, 'menu/ui/menu'
+
 class Main < Menu
+  attr_accessor :messenger
+
+  def initialize
+    @messenger = Messenger.new
+  end
+
+  def getMenu
+    self.clearConsole
+    puts <<header
+
+ .-.       .              .
+-|-| .-. .-|-. .-.-. .-..-| .-
+ ' '-`-`--'' '-`-`-`-'  `-'--'
+by dustin moorman
+
+***************************************
+
+1. Question Management
+2. Take a test
+3. See results
+4. exit
+
+header
+  end
+
+  def clearConsole
+    if OS.linux? or OS.mac? or OS.unix?
+      system 'clear'
+    elsif OS.windows?
+      system 'cls'
+    end
+  end
 end
