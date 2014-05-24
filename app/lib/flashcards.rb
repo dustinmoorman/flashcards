5e337b0fa29aa107068774ff10a36f9906cef36b
Dustin Moorman <dustin.moorman@gmail.com>
2014-05-24 01:15:28 -0500

    refactoring variable name

--- app/lib/flashcards.rb
+++ app/lib/flashcards.rb
@@ -5,23 +5,23 @@ autoload :Main, 'menu/ui/main'
 
 class Flashcards
   def Run
-    clui = Main.new
+    main = Main.new
 
     loop do
-      clui.getMenu
-      puts clui.messenger.getMessage
+      main.getMenu
+      puts main.messenger.getMessage
       uin = gets.chomp
       case uin
         when '1'
-          clui.messenger.push('user chose 1')
+          main.messenger.push('user chose 1')
         when '2'
-          clui.messenger.push('user chose 2')
+          main.messenger.push('user chose 2')
         when '3'
-          clui.messenger.push('user chose 3')
+          main.messenger.push('user chose 3')
         when '4'
           break
         else
-          clui.messenger.push('try again')
+          main.messenger.push('try again')
       end
     end
   end
