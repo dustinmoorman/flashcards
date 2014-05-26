e71731b0f5dc81338c0f2b71d0ebc76ea4ca17a3
Dustin Moorman <dustin.moorman@gmail.com>
2014-05-25 20:43:54 -0500

    correct abstraction!

--- app/lib/menu/ui/menu.rb
+++ app/lib/menu/ui/menu.rb
@@ -1,7 +1,13 @@
 autoload :OS, 'menu/tools/os'
+autoload :Messenger, 'menu/tools/messenger'
 
 class Menu
   include OS
+  attr_accessor :messenger
+
+  def initialize
+    @messenger = Messenger.new
+  end
 
   def clearConsole
     if OS::Detect.linux? or OS::Detect.mac? or OS::Detect.unix?
