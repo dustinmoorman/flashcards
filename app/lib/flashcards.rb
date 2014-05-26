aa862bcec2edb1f472b190c8554078cbbc51e373
Dustin Moorman <dustin.moorman@gmail.com>
2014-05-25 20:28:44 -0500

    making this even more correct!

--- app/lib/flashcards.rb
+++ app/lib/flashcards.rb
@@ -4,23 +4,25 @@
 autoload :Main, 'menu/ui/main'
 
 class Flashcards
+  def initialize
+    @main = Main.new
+  end
   def Run
-    main = Main.new
     loop do
-      main.getMenu
-      puts main.messenger.getMessage
+      @main.getMenu
+      puts @main.messenger.getMessage
       uin = gets.chomp
       case uin
         when '1'
-          main.messenger.push('user chose 1')
+          @main.messenger.push('user chose 1')
         when '2'
-          main.messenger.push('user chose 2')
+          @main.messenger.push('user chose 2')
         when '3'
-          main.messenger.push('user chose 3')
+          @main.messenger.push('user chose 3')
         when '4'
           break
         else
-          main.messenger.push('try again')
+          @main.messenger.push('try again')
       end
     end
   end
