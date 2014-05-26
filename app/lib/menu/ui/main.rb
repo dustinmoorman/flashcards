46fdbafc0a3d4844159bbd06704a946c2cdb4541
Dustin Moorman <dustin.moorman@gmail.com>
2014-05-26 01:40:56 -0500

    the situation so far

--- app/lib/menu/ui/main.rb
+++ app/lib/menu/ui/main.rb
@@ -1,24 +1,16 @@
 autoload :Menu, 'menu/ui/menu'
 
 class Main < Menu
+  def setOpts
+    @nav_opts['1'] = 'Manage questions'
+    @nav_opts['2'] = 'Take a test'
+    @nav_opts['3'] = 'See results'
+    @nav_opts['4'] = 'exit'
+  end
   def getMenu
     self.clearConsole
-    puts <<header
-
- .-.       .              .
--|-| .-. .-|-. .-.-. .-..-| .-
- ' '-`-`--'' '-`-`-`-'  `-'--'
-by dustin moorman
-
-===============================
-
-  :: Main
-
-1. Question Management
-2. Take a test
-3. See results
-4. exit
-
-header
+    @nav_opts.each do |key, opt|
+      puts "#{key}. #{opt}"
+    end
   end
 end
--- app/lib/menu/ui/menu.rb
+++ app/lib/menu/ui/menu.rb
@@ -4,16 +4,30 @@ autoload :Messenger, 'menu/tools/messenger'
 class Menu
   include OS
   attr_accessor :messenger
-
   def initialize
     @messenger = Messenger.new
+    @nav_opts = Hash.new
+    @location = 'Menu'
   end
-
   def clearConsole
     if OS::Detect.linux? or OS::Detect.mac? or OS::Detect.unix?
       system 'clear'
     elsif OS::Detect.windows?
       system 'cls'
     end
+  end
+  def getHeader
+  	header = <<hdr
+
+ .-.       .              .
+-|-| .-. .-|-. .-.-. .-..-| .-
+ ' '-`-`--'' '-`-`-`-'  `-'--'
+by dustin moorman
+
+===============================
+
+  :: 
+hdr
+    return header
   end
 end
