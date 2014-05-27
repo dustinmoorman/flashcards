a4d3bf7b707a1e2518794653caaa210249aea046
Dustin Moorman <dustin.moorman@gmail.com>
2014-05-26 21:13:11 -0500

    this is right

--- app/lib/menu/ui/main.rb
+++ app/lib/menu/ui/main.rb
@@ -1,8 +1,13 @@
 autoload :Menu, 'menu/ui/menu'
+autoload :Em, 'menu/ui/em'
 
 class Main < Menu
+  def initialize
+    super
+    @Em = Em.new
+  end
   def setOpts
-    @nav_opts['1'] = 'Manage tests'
+    @nav_opts['Manage tests'] = @Em
     @nav_opts['2'] = 'Take a test'
     @nav_opts['3'] = 'See results'
     @nav_opts['4'] = 'exit'
