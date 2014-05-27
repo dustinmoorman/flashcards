2840a423eee45eaa5f5fa42acad5b1a02a9dd98b
Dustin Moorman <dustin.moorman@gmail.com>
2014-05-26 20:42:37 -0500

    changing wording

--- app/lib/menu/ui/main.rb
+++ app/lib/menu/ui/main.rb
@@ -2,7 +2,7 @@ autoload :Menu, 'menu/ui/menu'
 
 class Main < Menu
   def setOpts
-    @nav_opts['1'] = 'Manage questions'
+    @nav_opts['1'] = 'Manage tests'
     @nav_opts['2'] = 'Take a test'
     @nav_opts['3'] = 'See results'
     @nav_opts['4'] = 'exit'
