ffbab9be14b1a494b6180c516423784c328c5d76
Dustin Moorman <dustin.moorman@gmail.com>
2014-05-26 20:31:24 -0500

    unnecessary

--- app/lib/flashcards.rb
+++ app/lib/flashcards.rb
@@ -2,13 +2,10 @@
 #dustinmoorman@gmail.com
 
 autoload :Main, 'menu/ui/main'
-autoload :Em, 'menu/ui/em'
 
 class Flashcards
   def initialize
     @main = Main.new
-    @em   = Em.new
-
   end
   def Run
       @main.getMenu
