8684b096fe893df136f55f98f8829a275fb647f9
Dustin Moorman <dustin.moorman@gmail.com>
2014-05-26 21:26:27 -0500

    doesn't look better now but when they are all diff menu objs it will

--- app/lib/menu/ui/main.rb
+++ app/lib/menu/ui/main.rb
@@ -2,15 +2,11 @@ autoload :Menu, 'menu/ui/menu'
 autoload :Em, 'menu/ui/em'
 
 class Main < Menu
-  def initialize
-    super
-    @Em = Em.new
-  end
   def setOpts
-    @nav_opts['Manage tests'] = @Em
-    @nav_opts['Take a test'] = @Em #pluggin for now
-    @nav_opts['See results'] = @Em #pluggin for now
-    @nav_opts['exit'] = @Em #pluggin for now
+    @nav_opts['Manage tests'] = Em.new
+    @nav_opts['Take a test'] = Em.new #pluggin for now
+    @nav_opts['See results'] = Em.new #pluggin for now
+    @nav_opts['exit'] = Em.new #pluggin for now
     @location = 'Main'
   end
 end
