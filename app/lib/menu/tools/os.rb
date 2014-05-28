8f0a0175b615cd9027fe67a0290a9a3bc5443e72
dustinmoorman <dustin.moorman@gmail.com>
2014-05-28 12:46:37 -0500

    absolving ws

--- app/lib/menu/tools/os.rb
+++ app/lib/menu/tools/os.rb
@@ -6,15 +6,12 @@ module OS
     def self.windows?
       (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
     end
-
     def self.mac?
       (/darwin/ =~ RUBY_PLATFORM) != nil
     end
-
     def self.unix?
       !self.windows?
     end
-
     def self.linux?
       self.unix? and not self.mac?
     end
