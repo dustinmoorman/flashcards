b20ada531428b541f26b3a4987165935e6e04f32
Dustin Moorman <dustin.moorman@gmail.com>
2014-05-30 23:26:55 -0500

    module removal

--- app/lib/menu/ui/menu.rb
+++ app/lib/menu/ui/menu.rb
@@ -1,52 +1,52 @@
 require_relative '../tools/os'
 require_relative '../tools/messenger'
-module Flashcards
-  class Ui
-    class Menu
-      include OS
-      attr_accessor :messenger
-      def initialize
-        @messenger = Messenger.new
-        @nav_opts = Hash.new
-        @location = 'Menu'
+
+class Ui
+  class Menu
+    include OS
+    attr_accessor :messenger
+    def initialize
+      @messenger = Messenger.new
+      @nav_opts = Hash.new
+      @location = 'Menu'
+    end
+    def clearConsole
+      if OS::Detect.linux? or OS::Detect.mac? or OS::Detect.unix?
+        system 'clear'
+      elsif OS::Detect.windows?
+        system 'cls'
       end
-      def clearConsole
-        if OS::Detect.linux? or OS::Detect.mac? or OS::Detect.unix?
-          system 'clear'
-        elsif OS::Detect.windows?
-          system 'cls'
+    end
+    def getMenu
+      loop do
+        self.clearConsole
+        self.setOpts
+        print self.getHeader
+        pos = 1
+        commands = Hash.new
+        @nav_opts.each do |desc, concreteMenu|
+          puts pos.to_s + " #{desc}"
+          commands[pos] = concreteMenu
+          pos += 1
+        end
+        puts @messenger.getMessage
+        uin = gets.chomp
+        case uin
+          when '1'
+            commands[1].getMenu
+          when '2'
+            commands[2].getMenu
+          when '3'
+            commands[3].getMenu
+          when '4'
+            break
+          else
+            @messenger.push('try again')
         end
       end
-      def getMenu
-        loop do
-          self.clearConsole
-          self.setOpts
-          print self.getHeader
-          pos = 1
-          commands = Hash.new
-          @nav_opts.each do |desc, concreteMenu|
-            puts pos.to_s + " #{desc}"
-            commands[pos] = concreteMenu
-            pos += 1
-          end
-          puts @messenger.getMessage
-          uin = gets.chomp
-          case uin
-            when '1'
-              commands[1].getMenu
-            when '2'
-              commands[2].getMenu
-            when '3'
-              commands[3].getMenu
-            when '4'
-              break
-            else
-              @messenger.push('try again')
-          end
-        end
-      end
-      def getHeader
-        header = <<hdr
+    end
+    def getHeader
+      header = <<hdr
 
  .-.       .              .
 -|-| .-. .-|-. .-.-. .-..-| .-
@@ -58,8 +58,8 @@ by dustin moorman
   :: #{@location}
 
 hdr
-        return header
-      end
+      return header
     end
   end
 end
+
