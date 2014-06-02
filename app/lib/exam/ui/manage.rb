45d976c4d7452c3e4f0f723d5920f84e90dd6a8c
dustinmoorman <dustin.moorman@gmail.com>
2014-06-02 09:59:26 -0500

    moving exam management to the proper namespace

--- app/lib/exam/ui/manage.rb
+++ app/lib/exam/ui/manage.rb
@@ -1,2 +1,11 @@
-class Manage
+require 'menu/ui/menu'
+
+class Manage < Ui::Menu
+  def self.setOpts
+    @nav_opts['List Exams'] = self #str8 pluqqin
+    @nav_opts['Add Exam'] = self #str8 pluqqin
+    @nav_opts['Remove Exam'] = self
+    @nav_opts['back'] = self
+    @location = 'Exam Management'
+  end
 end
