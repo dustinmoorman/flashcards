8d6298bf655a95dafee57849213421bfae6b7ec5
dustinmoorman <dustin.moorman@gmail.com>
2014-06-02 09:57:56 -0500

    moving question management to the proper namespace

--- app/lib/exam/questions/ui/manage.rb
+++ app/lib/exam/questions/ui/manage.rb
@@ -1,2 +1,11 @@
-class Manage
+require 'menu/ui/menu'
+
+class Manage < Ui::Menu
+  def setOpts
+    @nav_opts['1'] = 'List Questions'
+    @nav_opts['2'] = 'Add Question'
+    @nav_opts['3'] = 'Remove Question'
+    #@nav_opts['4'] = self.back @TODO Implement historian & back method for menu
+    @location = 'Question Management'
+  end
 end
--- app/lib/menu/ui/exam.rb
+++ app/lib/menu/ui/exam.rb
@@ -10,13 +10,4 @@ module Mgmt
       @location = 'Exam Management'
     end
   end
-  class Qm < Menu
-    def setOpts
-      @nav_opts['1'] = 'List Questions'
-      @nav_opts['2'] = 'Add Question'
-      @nav_opts['3'] = 'Remove Question'
-      @nav_opts['4'] = 'back'
-      @location = 'Question Management'
-    end
-  end
 end
