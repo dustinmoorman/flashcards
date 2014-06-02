4ef4353a0db2f7dcd59ca151aa0297d5615cdb26
dustinmoorman <dustin.moorman@gmail.com>
2014-06-02 09:49:02 -0500

    getting the ball rolling here

--- app/lib/menu/ui/main.rb
+++ app/lib/menu/ui/main.rb
@@ -2,10 +2,10 @@ require_relative 'menu'
 
 class Main < Ui::Menu
   def setOpts
-    @nav_opts['Manage exams'] = self
-    @nav_opts['Take an exam'] = self
-    @nav_opts['See results'] = self
-    @nav_opts['exit'] = self
+    @nav_opts['Manage exams'] = Exam::Ui::Manage.new
+    @nav_opts['Take an exam'] = Exam::Ui::Take.new
+    @nav_opts['See results'] = Exam::Ui::Results.new
+    @nav_opts['exit'] = self.exit
     @location = 'Main'
   end
 end
