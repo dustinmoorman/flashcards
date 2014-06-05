d30aebbf907ad83976385cc17405b0e65233b0c0
dustinmoorman <dustin.moorman@gmail.com>
2014-06-05 13:40:04 -0500

    one day i will achieve proper namespacing

--- app/lib/menu/ui/menu.rb
+++ app/lib/menu/ui/menu.rb
@@ -1,57 +1,57 @@
 require_relative '../tools/os'
 require_relative '../tools/messenger'
-
-module Ui
-  class Menu
-    include OS
-    attr_accessor :messenger
-    def initialize
-      @messenger = ::Menu::Tools::Messenger.new
-      @nav_opts = Hash.new
-      @location = 'Menu'
-    end
-    def clearConsole
-      if OS::Detect.linux? or OS::Detect.mac? or OS::Detect.unix?
-        system 'clear'
-      elsif OS::Detect.windows?
-        system 'cls'
+module Menu
+  module Ui
+    class Menu
+      include OS
+      attr_accessor :messenger
+      def initialize
+        @messenger = ::Menu::Tools::Messenger.new
+        @nav_opts = Hash.new
+        @location = 'Menu'
       end
-    end
-    def getMenu
-      loop do
-        self.clearConsole
-        self.setOpts
-        print self.getHeader
-        pos = 1
-        commands = Hash.new
-        @nav_opts.each do |desc, concreteMenu|
-          puts pos.to_s + " #{desc}"
-          commands[pos.to_s] = concreteMenu
-          pos += 1
+      def clearConsole
+        if OS::Detect.linux? or OS::Detect.mac? or OS::Detect.unix?
+          system 'clear'
+        elsif OS::Detect.windows?
+          system 'cls'
         end
-        puts @messenger.getMessage        
-	      self.processInput(gets.chomp, commands)
       end
-    end
-    def processInput(uin, commands) 
-      if commands.key? uin
-        if commands[uin].respond_to? 'getMenu'
-          # @historian.push(commands[uin]) @TODO implement
-          commands[uin].getMenu
-        elsif commands[uin] == 'exit'
-          self.exit
-        elsif commands[uin] == 'back'
-          # @historian.back @TODO implement
+      def getMenu
+        loop do
+          self.clearConsole
+          self.setOpts
+          print self.getHeader
+          pos = 1
+          commands = Hash.new
+          @nav_opts.each do |desc, concreteMenu|
+            puts pos.to_s + " #{desc}"
+            commands[pos.to_s] = concreteMenu
+            pos += 1
+          end
+          puts @messenger.getMessage
+          self.processInput(gets.chomp, commands)
         end
-      else
-        @messenger.push('try again')
       end
-    end
-    def exit
-      exit!
-    end
-    def getHeader
-      header = <<hdr
+      def processInput(uin, commands)
+        if commands.key? uin
+          if commands[uin].respond_to? 'getMenu'
+            # @historian.push(commands[uin]) @TODO implement
+            commands[uin].getMenu
+          elsif commands[uin] == 'exit'
+            self.exit
+          elsif commands[uin] == 'back'
+            # @historian.back @TODO implement
+          end
+        else
+          @messenger.push('try again')
+        end
+      end
+      def exit
+        exit!
+      end
+      def getHeader
+        header = <<hdr
 
  .-.       .              .
 -|-| .-. .-|-. .-.-. .-..-| .-
@@ -63,8 +63,8 @@ by dustin moorman
   :: #{@location}
 
 hdr
-      return header
+        return header
+      end
     end
   end
 end
-
