3ee2082cc8a19d35bdd68db7472a44b004ad29c6
dustinmoorman <dustin.moorman@gmail.com>
2014-06-05 13:41:30 -0500

    refactoring exam::take extension of menu

--- app/lib/exam/ui/take.rb
+++ app/lib/exam/ui/take.rb
@@ -2,7 +2,7 @@ require 'menu/ui/menu'
 
 module Exam
   module Ui
-    class Take < ::Ui::Menu
+    class Take < ::Menu::Ui::Menu
       def setOpts
         @nav_opts['List Exams'] = self #str8 pluqqin @TODO this will probably list automatically
         @nav_opts['back'] = self
