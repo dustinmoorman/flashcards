2ffa134891c2534357ab8650bfdcba445ddb87e5
dustinmoorman <dustin.moorman@gmail.com>
2014-06-05 09:48:04 -0500

    building out exam::ui::results

--- app/lib/exam/ui/results.rb
+++ app/lib/exam/ui/results.rb
@@ -1,2 +1,13 @@
-class Results
+require 'menu/ui/menu'
+
+module Exam
+  module Ui
+    class Results < ::Ui::Menu
+      def setOpts
+        @nav_opts['Select Exam'] = self #str8 pluqgin
+        @nav_opts['back'] = self
+        @location = 'Exam Results'
+      end
+    end
+  end
 end
