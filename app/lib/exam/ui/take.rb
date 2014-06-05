3cfd6e45053229b9777c240de14b3ec26b0aa544
dustinmoorman <dustin.moorman@gmail.com>
2014-06-05 10:13:13 -0500

    will  need some overloading for getMenu on these lower levels

--- app/lib/exam/ui/take.rb
+++ app/lib/exam/ui/take.rb
@@ -1,2 +1,13 @@
-class Take
+require 'menu/ui/menu'
+
+module Exam
+  module Ui
+    class Take < ::Ui::Menu
+      def setOpts
+        @nav_opts['List Exams'] = self #str8 pluqqin @TODO this will probably list automatically
+        @nav_opts['back'] = self
+        @location = 'Take Exam'
+      end
+    end
+  end
 end
