c8712a394c442547ee0b0018f468b239de02d413
dustinmoorman <dustin.moorman@gmail.com>
2014-06-05 13:32:44 -0500

    namespace fix in messenger, Menu::Tools::Messenger

--- app/lib/menu/tools/messenger.rb
+++ app/lib/menu/tools/messenger.rb
@@ -1,8 +1,12 @@
-class Messenger
-  def push(x)
-    @message = x
-  end
-  def getMessage
-    return @message
+module Menu
+  module Tools
+    class Messenger
+      def push(x)
+        @message = x
+      end
+      def getMessage
+        return @message
+      end
+    end
   end
 end
