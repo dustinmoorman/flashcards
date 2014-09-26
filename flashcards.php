0bd05c42710f79d5f9905176fd422dacda9afb24
Dustin Moorman <dustin.moorman@gmail.com>
2014-09-25 20:43:59 -0500

    notify

--- flashcards.php
+++ flashcards.php
@@ -5,6 +5,8 @@ if (isset($argv[1])) {
 		&& (strpos($argv[1], '.php') 
 			|| strpos($argv[1], '.json'))) {
 		
+	} else {
+		echo "Invalid question file";
 	}
 } else {
 	echo "No questions provided.";
