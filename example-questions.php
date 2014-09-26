62eaa52ffe00abbec92e2b38e833e4c159199df3
Dustin Moorman <dustin.moorman@gmail.com>
2014-09-25 21:00:07 -0500

    adding sample questions

---
+++ example-questions.php
@@ -0,0 +1,16 @@
+return [
+	[
+		'question' => 'What is the programming language this application is written in?',
+		'answer' => 'PHP',
+		'incorrect' => 'C++',
+		'incorrect' => 'D',
+		'incorrect' => 'Erlang'
+	],
+	[
+		'question' => 'What is the name of the company that maintains PHP?',
+		'answer' => 'Zend',
+		'incorrect' => 'IBM',
+		'incorrect' => 'Engine Yard',
+		'incorrect' => 'eBay'
+	]
+];
