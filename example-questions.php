9928ff5f695c7cc06e1de04b099d79e81d72049c
Dustin Moorman <dustin.moorman@gmail.com>
2014-09-25 21:07:06 -0500

    doing it this way

--- example-questions.php
+++ example-questions.php
@@ -1,16 +1,13 @@
+<?php
 return [
 	[
 		'question' => 'What is the programming language this application is written in?',
 		'answer' => 'PHP',
-		'incorrect' => 'C++',
-		'incorrect' => 'D',
-		'incorrect' => 'Erlang'
+		'incorrect' => ['C++','D','Erlang']
 	],
 	[
 		'question' => 'What is the name of the company that maintains PHP?',
 		'answer' => 'Zend',
-		'incorrect' => 'IBM',
-		'incorrect' => 'Engine Yard',
-		'incorrect' => 'eBay'
+		'incorrect' => ['IBM','Engine Yard','eBay']
 	]
 ];
