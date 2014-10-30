fb97c2594b9320bc6f7b765b19e7c70bae5d4bbb
Dustin Moorman <dustin.moorman@gmail.com>
2014-10-30 16:37:16 -0500

    doing work

--- Flashcards/Exam/Timer.php
+++ Flashcards/Exam/Timer.php
@@ -4,18 +4,26 @@ namespace Flashcards\Exam;
 
 class Timer
 {
+	protected static $time;
+
 	public static function start()
 	{
-
+		list($usec, $sec) = self::getTime();
+		$mtime =  $sec + $usec;
 	}
 
 	public static function stop()
 	{
-
+		
 	}
 
 	public static function reset()
 	{
+		self::$time = 0;
+	}
 
+	protected static function getTime()
+	{
+		return explode(' ', microtime());
 	}
 }
